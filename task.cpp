ff3bb1d96faffae1b69e266b58f149db5afee661
Tyler Wilson <tylerjaywilson@gmail.com>
2016-02-16 13:35:02 -0700

    adding all initial files

---
+++ task.cpp
@@ -0,0 +1,40 @@
+/*
+* Athour: Jorden Luke & Tyler Wilson
+* Date: 02/16/2016
+* Notes: This class is used to hold the data for task. It was used as part of 
+* Lab 2 for real time processors.
+*/
+
+
+#include "task.hpp"
+
+Task::Task(int id_t, int time, int period_t)
+{
+  id = id_t;
+  extime = time;
+  period = period_t;
+} 
+int Task::get_id()
+{
+  return id;
+}
+void Task::set_id(int id_t)
+{
+  id = id_t;
+}
+int Task::get_extime()
+{
+  return extime;
+}
+void Task::set_extime(int time)
+{
+  extime = time;
+}
+int Task::get_period()
+{
+  return period;
+}
+void Task::set_period(int period_t)
+{
+  period = period_t;
+}
---
+++ task.hpp
@@ -0,0 +1,19 @@
+#ifndef TASK_HPP 
+#define TASK_HPP
+
+class Task{
+
+  int id;
+  int extime;
+  int period;
+
+public:
+  Task(int id_t, int extime, int period); 
+  int get_id();
+  void set_id(int id_t);
+  int get_extime();
+  void set_extime(int time);
+  int get_period();
+  void set_period(int period_t);
+}
+#endif
